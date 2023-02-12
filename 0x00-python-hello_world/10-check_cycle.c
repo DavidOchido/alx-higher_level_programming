6718323db014330c9ef2d2fe3d614b0ee1c20a85
DavidOchido <davidochido1@gmail.com>
2023-02-12 05:11:22 -0800

    Hello World

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -11,25 +11,26 @@
 
 int check_cycle(listint_t *list)
 {
-	listint_t *fast = list->next;
-	listint_t *slow = list;
+        listint_t *fast = list->next;
+        listint_t *slow = list;
 
-	if (list == NULL)
-	{
-		return (0);
-	}
+        if (list == NULL)
+        {
+                return (0);
+        }
 
-	while (fast != NULL && fast->next != NULL && slow != NULL)
-	{
-		if (fast == slow)
-		{
-			return (1);
-		}
+        while (fast != NULL && fast->next != NULL && slow != NULL)
+        {
+                if (fast == slow)
+                {
+                        return (1);
+                }
 
-		fast = fast->next->next;
-		slow = slow->next;
+                fast = fast->next->next;
+                slow = slow->next;
 
-	}
+        }
 
-	return (0);
+        return (0);
 }
+
