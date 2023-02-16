ff35b7ef5a6d32840881d1abcdbc3ed6172e12a9
DavidOchido <davidochido1@gmail.com>
2023-02-16 15:26:52 -0800

    python-if_else_loops_functions

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-for ch in range(0, 99):
-    print("{:d} = 0x{:x}".format(ch, ch))
+for i in range(0, 99):
+    print("{:d} = {}".format(i, hex(i)))
--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-for x in range(0, 100):
-    if x != 99:
-        print("{:02d}, ".format(x), end='')
-    else:
-        print("{:02d}".format(x))
+for i in range(99):
+    print("{:02d}".format(i), end=", ")
+else:
+    print("{:02d}".format(i + 1))
