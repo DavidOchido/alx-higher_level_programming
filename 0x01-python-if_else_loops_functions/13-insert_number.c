b5d027a59c9a0c46a8048df4a8b704d27695f01c
DavidOchido <davidochido1@gmail.com>
2024-06-03 00:45:10 -0700

    13. Insert in sorted linked list

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -2,23 +2,6 @@
 #define LISTS_H
 
 #include <stdlib.h>
-
-/**
- * struct listint_s - singly linked list
- * @n: integer
- * @next: points to the next node
- *
- * Description: singly linked list node structure
- */
-typedef struct listint_s
-{
-    int n;
-    struct listint_s *next;
-} listint_t;
-
-listint_t *insert_node(listint_t **head, int number);
-
-#endif /* LISTS_H */
 #include "lists.h"
 
 /**
